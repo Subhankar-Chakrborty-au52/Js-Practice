fa094d051fe0668abfbff6d8a684e1252aed9a34
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2024-07-16 15:01:34 +0530

    loops in js

---
+++ chaicode30dayschallenge/index.js
@@ -0,0 +1,65 @@
+// //Arithmatic operations
+
+// console.log(5 + 4);
+
+// console.log(8 - 5);
+
+// console.log(6 * 5);
+
+// console.log(8 / 2);
+
+// console.log(9 % 2);
+
+//Assignment Operators
+
+// let number = 5;
+// number += 4;
+// console.log(number);
+
+//comparison operators
+
+// const num1 = 10;
+// const num2 = 4;
+// console.log(num1 > num2);
+
+// const num1 = 10;
+// const num2 = 4;
+// console.log(num1 < num2);
+
+// const num1 = 10;
+// const num2 = 10;
+// console.log(num1 <= num2);
+
+// const num1 = 10;
+// const num2 = 10;
+// console.log(num1 >= num2);
+
+// const num1 = 10;
+// const num2 = 10;
+// console.log(num1 == num2);
+
+// const num1 = "10";
+// const num2 = 10;
+// console.log(num1 === num2);
+
+// const findFactorial = (num) => {
+//   if (num == 0 || num == 1) {
+//     return 1;
+//   } else {
+//     return num * findFactorial(num - 1);
+//   }
+// };
+// console.log(findFactorial(7));
+
+// Loops
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+const arr = [];
+
+for (let i = 1; i <= 100; i++) {
+  arr.push(5 * i);
+}
+
+console.log(arr);
